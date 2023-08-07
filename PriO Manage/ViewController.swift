c84f7db749e97af2e5fd0bdab61e8c4c68efab7a
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-07 20:31:42 +0700

    Initial Commit

---
+++ PriO Manage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PriO Manage
+//
+//  Created by Khoa Dai on 07/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
