c099b770c521c446b34065d9f0552242fa9d16e2
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-25 11:04:03 +0700

    Center the container view horizontally and vertically in the layout frame.

--- PriO Manage/ListViewController/ProgressHeraderView.swift
+++ PriO Manage/ListViewController/ProgressHeraderView.swift
@@ -24,7 +24,11 @@ class ProgressHeaderView: UICollectionReusableView {
         upperView.translatesAutoresizingMaskIntoConstraints = false
         containerView.translatesAutoresizingMaskIntoConstraints = false
         
+        // Maintain a 1:1 fixed aspect ratio for the superview and container views.
         heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
         containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1).isActive = true
+        
+        containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
 }
