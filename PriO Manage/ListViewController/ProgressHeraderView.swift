4e9da2cddb47c5a5d1c83b072a071859d64e4344
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-25 10:53:59 +0700

    Add private properties to create upperView, lowerView, and containerView subviews. Initialize each property with a zero-sized frame.

--- PriO Manage/ListViewController/ProgressHeraderView.swift
+++ PriO Manage/ListViewController/ProgressHeraderView.swift
@@ -10,4 +10,8 @@ import UIKit
 class ProgressHeaderView: UICollectionReusableView {
     
     var progress: CGFloat = 0
+    
+    private let upperView = UIView(frame: .zero)
+    private let lowerView = UIView(frame: .zero)
+    private let containerView = UIView(frame: .zero)
 }
