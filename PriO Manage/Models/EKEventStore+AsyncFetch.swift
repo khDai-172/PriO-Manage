bd7488a5efc6e194ea54d0b1845d18f687adf6b2
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 09:41:49 +0700

    Upon success, resume the continuation, returning the reminders.

--- PriO Manage/Models/EKEventStore+AsyncFetch.swift
+++ PriO Manage/Models/EKEventStore+AsyncFetch.swift
@@ -13,6 +13,7 @@ import Foundation
 // You use continuations to wrap concurrent callback functions so that their results can be returned inline. The await keyword indicates that your task suspends until the continuation resumes.
 // Call the EventKit method fetchReminders(matching:). The method passes the matching reminders to the completion handler.
 // if let reminders is the shortened Swift syntax for if let reminders = reminders.
+// Your function resumes execution and returns the EKReminder array. Your function resumes execution and returns the EKReminder array.
 
 extension EKEventStore {
     
@@ -20,7 +21,7 @@ extension EKEventStore {
         try await withCheckedContinuation { continuation in
             fetchReminders(matching: predicate) { reminders in
                 if let reminders {
-                    
+                    continuation.resume(returning: reminders)
                 } else {
                     
                 }
