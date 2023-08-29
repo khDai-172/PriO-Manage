37b6963ffe4f763df5967b942e8e63fed0b8708a
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 20:49:26 +0700

    Create a private property named ekStore, and assign it a new EKEventStore.

--- PriO Manage/Models/ReminderStore.swift
+++ PriO Manage/Models/ReminderStore.swift
@@ -10,9 +10,10 @@ import Foundation
 
 // Creating a reminder store abstraction to facilitate the persistence of reminders data.
 // You can’t override methods in final classes. The compiler will display a warning if you try to subclass ReminderStore.
-// You’ll create a single instance of your class to use throughout your app.
+// Create a static property named shared, and assign it a new ReminderStore.
 
 final class ReminderStore {
     static let shared = ReminderStore()
     
+    private let ekStore = EKEventStore()
 }
