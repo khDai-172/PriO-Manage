6ecf702a716882dc72ef384f0bcf4661a975391b
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 09:22:19 +0700

    Call the generic withCheckedThrowingContinuation(_:) Swift function.

--- PriO Manage/Models/EKEventStore+AsyncFetch.swift
+++ PriO Manage/Models/EKEventStore+AsyncFetch.swift
@@ -8,11 +8,15 @@
 import EventKit
 import Foundation
 
-//EKEventStore objects can access a user’s calendar events and reminders.
+// EKEventStore objects can access a user’s calendar events and reminders.
+// The async keyword after the function’s parameters indicates that this function can execute asynchronously.
+// You use continuations to wrap concurrent callback functions so that their results can be returned inline. The await keyword indicates that your task suspends until the continuation resumes.
 
 extension EKEventStore {
     
     func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
-        
+        try await withCheckedContinuation { continuation in
+            
+        }
     }
 }
