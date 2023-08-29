4d17817b650c5f2e529bd0fd2cc2e3b5c20eadfc
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 15:23:27 +0700

    Assign the calendar identifier to the id property.

--- PriO Manage/Models/Reminder+EKReminder.swift
+++ PriO Manage/Models/Reminder+EKReminder.swift
@@ -8,10 +8,12 @@
 import EventKit
 import Foundation
 
+// If a reminder has an alarm, the system presents a notification to the user when the reminder is due.
 extension Reminder {
     init(with ekReminder: EKReminder) throws {
         guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
             throw PrioError.reaminderHasNoDueDate
         }
+        id = ekReminder.calendarItemIdentifier
     }
 }
