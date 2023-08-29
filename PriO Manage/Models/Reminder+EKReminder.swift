71410ec2a24a90241662871da0a98e2eb73df139
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 15:30:57 +0700

    Assign the title, dueDate, notes, and isComplete properties.

--- PriO Manage/Models/Reminder+EKReminder.swift
+++ PriO Manage/Models/Reminder+EKReminder.swift
@@ -9,11 +9,16 @@ import EventKit
 import Foundation
 
 // If a reminder has an alarm, the system presents a notification to the user when the reminder is due.
+// EventKit provides a unique identifier for all reminders.
 extension Reminder {
     init(with ekReminder: EKReminder) throws {
         guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
             throw PrioError.reaminderHasNoDueDate
         }
         id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
     }
 }
