fb34265d63b1733aa3c0152468c52abf004836a9
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 20:45:39 +0700

    Create a static property named shared, and assign it a new ReminderStore.

--- PriO Manage/Models/ReminderStore.swift
+++ PriO Manage/Models/ReminderStore.swift
@@ -10,7 +10,9 @@ import Foundation
 
 // Creating a reminder store abstraction to facilitate the persistence of reminders data.
 // You can’t override methods in final classes. The compiler will display a warning if you try to subclass ReminderStore.
+// You’ll create a single instance of your class to use throughout your app.
 
 final class ReminderStore {
+    static let shared = ReminderStore()
     
 }
