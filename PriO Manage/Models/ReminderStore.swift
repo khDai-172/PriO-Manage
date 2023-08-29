326255098fbc454eef504a5ab6914c096f068dd9
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 19:43:45 +0700

    In the Models group, create a new Swift file named ReminderStore.swift that imports EventKit. Create a class named ReminderStore, and mark it as final. You’ll begin creating a reminder store abstraction to facilitate the persistence of reminders data.

---
+++ PriO Manage/Models/ReminderStore.swift
@@ -0,0 +1,16 @@
+//
+//  ReminderStore.swift
+//  PriO Manage
+//
+//  Created by Khoa Dai on 29/08/2023.
+//
+
+import EventKit
+import Foundation
+
+// Creating a reminder store abstraction to facilitate the persistence of reminders data.
+// You can’t override methods in final classes. The compiler will display a warning if you try to subclass ReminderStore.
+
+final class ReminderStore {
+    
+}
