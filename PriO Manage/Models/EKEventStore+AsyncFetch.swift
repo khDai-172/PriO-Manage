4df28aa612be80196946e1e25e9f03ee5fdd417c
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 10:06:13 +0700

    change withCheckedContinuation func to withCheckedThrowingContinuation and upon failure, resume the continuation, throwing the error.

--- PriO Manage/Models/EKEventStore+AsyncFetch.swift
+++ PriO Manage/Models/EKEventStore+AsyncFetch.swift
@@ -18,12 +18,12 @@ import Foundation
 extension EKEventStore {
     
     func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
-        try await withCheckedContinuation { continuation in
+        try await withCheckedThrowingContinuation { continuation in
             fetchReminders(matching: predicate) { reminders in
                 if let reminders {
                     continuation.resume(returning: reminders)
                 } else {
-                    
+                    continuation.resume(throwing: PrioError.failedReadingReminders)
                 }
             }
         }
