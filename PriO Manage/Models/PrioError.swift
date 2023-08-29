75143a660e18661db175d608b87a632999968fbb
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 09:08:45 +0700

    Add an errorDescription property that describes the error.

--- PriO Manage/Models/PrioError.swift
+++ PriO Manage/Models/PrioError.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 // Types conforming to LocalizedError can provide localized messages that describe their errors and why they occur.
+// The LocalizedError protocol provides a default implementation of the errorDescription property that returns a nonspecific message.
 enum PrioError: LocalizedError {
     case failedReadingReminders
+    
+    var errorDescription: String? {
+        switch self {
+        case .failedReadingReminders:
+            return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
+        }
+    }
 }
