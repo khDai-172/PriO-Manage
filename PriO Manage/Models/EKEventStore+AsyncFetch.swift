acab9bd66058f125a6974b4ccf0e2276c8b827d0
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-29 09:28:41 +0700

    Write an if-let conditional statement that binds the reminders to a local constant.

--- PriO Manage/Models/EKEventStore+AsyncFetch.swift
+++ PriO Manage/Models/EKEventStore+AsyncFetch.swift
@@ -11,13 +11,19 @@ import Foundation
 // EKEventStore objects can access a user’s calendar events and reminders.
 // The async keyword after the function’s parameters indicates that this function can execute asynchronously.
 // You use continuations to wrap concurrent callback functions so that their results can be returned inline. The await keyword indicates that your task suspends until the continuation resumes.
+// Call the EventKit method fetchReminders(matching:). The method passes the matching reminders to the completion handler.
+// if let reminders is the shortened Swift syntax for if let reminders = reminders.
 
 extension EKEventStore {
     
     func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
         try await withCheckedContinuation { continuation in
             fetchReminders(matching: predicate) { reminders in
-                
+                if let reminders {
+                    
+                } else {
+                    
+                }
             }
         }
     }
