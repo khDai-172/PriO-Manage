cad4c61fc20a3e4aa41af93b53c83490350ce0e4
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-31 10:31:09 +0700

    Assign the store’s defaultCalendarForNewReminders() to the calendar property.

--- PriO Manage/Models/EKReminder+Reminder.swift
+++ PriO Manage/Models/EKReminder+Reminder.swift
@@ -8,10 +8,15 @@
 import EventKit
 import Foundation
 
+// EventKit calendar items must be associated with a calendar. The user can change their default calendar in the Settings app.
+// EventKit uses a combination of alarms and the due date to determine when to remind a user of an action. Today uses only the due date. You need to remove any excess alarms from the record.
+
 extension EKReminder {
     func update(using reminder: Reminder, in store: EKEventStore) {
         title = reminder.title
         notes = reminder.notes
         isCompleted = reminder.isComplete
+        calendar = store.defaultCalendarForNewReminders()
+        
     }
 }
