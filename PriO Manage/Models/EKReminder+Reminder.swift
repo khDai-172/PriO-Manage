b9462277fabb8c6b78f72672a98b7acd479cbc35
khDai-172 <ngkhoadai.edofhanu@gmail.com>
2023-08-31 10:41:10 +0700

    If the reminder has no alarms, add an alarm for the due date.

--- PriO Manage/Models/EKReminder+Reminder.swift
+++ PriO Manage/Models/EKReminder+Reminder.swift
@@ -10,6 +10,7 @@ import Foundation
 
 // EventKit calendar items must be associated with a calendar. The user can change their default calendar in the Settings app.
 // EventKit uses a combination of alarms and the due date to determine when to remind a user of an action. Today uses only the due date. You need to remove any excess alarms from the record.
+// The reminder must have one alarm in order to trigger a system notification when it’s due.
 
 extension EKReminder {
     func update(using reminder: Reminder, in store: EKEventStore) {
@@ -23,7 +24,9 @@ extension EKReminder {
             if comparison != .orderedSame {
                 removeAlarm(alarm)
             }
-            
+            if !hasAlarms {
+                addAlarm(EKAlarm(absoluteDate: reminder.dueDate))
+            }
         }
     }
 }
